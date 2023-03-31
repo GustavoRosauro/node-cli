42c71742876f223335c57a313232393d53a453f3
GustavoRosauro <gustavosrosauro@gmail.com>
2023-03-31 11:58:09 -0300

    adicionado novo comando

--- bin/index.js
+++ bin/index.js
@@ -1,8 +1,15 @@
 #!/usr/bin/env node
+const { exec } = require('child_process');
 let index = process.argv.indexOf('--name');
 let arg = '';
 if (index > -1) {
     arg = process.argv[index + 1];
+    console.log(arg)
+    if (arg.toLowerCase() == 'desligar')
+        exec('shutdown /s');
+    else if (arg.toLowerCase() == 'version') {
+        console.log('version 1.0.1');
+    }
 }
 
 console.log('O nome informado foi: ' + arg);
